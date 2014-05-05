8d73c673c418d83b6f2b0ab836ff4461d2f1736a
wangxipu <wangxipu@gmail.com>
2014-05-05 20:06:04 +0800

    add some answers on day 5

---
+++ 1027.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+d = {                                                                            
+    0 : '0', 1 : '1', 2 : '2', 3 : '3', 4 : '4',                                     
+    5 : '5', 6 : '6', 7 : '7', 8 : '8', 9 : '9',                                     
+    10 : 'A', 11 : 'B', 12 : 'C' }
+
+def getColorinMars(num):
+    li = []
+    D = 13
+    quo, rem = divmod(num, D)
+    while quo != 0:
+        li.append(d[rem])
+        quo, rem = divmod(quo, D)
+        
+    li.append(d[rem])
+    li.reverse()
+    return '0' * (2 - len(li)) + ''.join(li)
+
+if __name__ == '__main__':
+    r, g, b = [ int(x) for x in raw_input().split() ]
+    print '#%s%s%s' % (getColorinMars(r), 
+                       getColorinMars(g), 
+                       getColorinMars(b)) 
